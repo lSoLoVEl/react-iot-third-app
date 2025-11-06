414e68f20fb1c3020e64b6febeab48a5f3bcc98d
lSoLoVEl <s6519410044@sau.ac.th>
2025-11-06 12:28:58 +0700

    fix route

--- src/components/Navbariot.jsx
+++ src/components/Navbariot.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 export default function Navbariot() {
   return (
     <div className='flex justify-center gap-5 p-5 bg-blue-700 text-white'>
-        <Link to="/pageA"> หน้า A </Link>
+        <Link to="/"> หน้า A </Link>
         <Link to="/pageB"> หน้า B </Link>
         <Link to="/pageC"> หน้า C </Link>
         <Link to="/pageD"> หน้า D </Link>
