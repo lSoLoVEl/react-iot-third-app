64040d8b1750bd5b76a0d52a25b7dee1cb1a4bfc
lel <fakemail@gmail.com>
2025-11-13 11:43:01 +0700

    third commit cal

--- src/pages/PageA.jsx
+++ src/pages/PageA.jsx
@@ -3,8 +3,54 @@ import Headeriot from "../components/Headeriot";
 import Footeriot from "../components/Footeriot";
 import Navbariot from "../components/Navbariot";
 import marketShare from "../assets/marketShare.png";
+import { useState } from "react";
+import Swal from 'sweetalert2'
 
 export default function PageA() {
+  // สร้างตัวแปร state
+  const [moneyPay, setMoneyPay] = useState('');
+  const[person,setPerson] = useState('');
+  const [moneyShare,setMoneyShare] = useState('0.00');
+
+// สร้างฟังก์ชันคำนวณ
+  const handleCalMoneyShareClick = () => {
+    // Validate UI
+    if(moneyPay ==''){
+      // alert('กรุณาป้อนเงิน')
+      Swal.fire({
+        title:'คำเตือน',
+        text:'กรุณาป้อนเงิน',
+        icon:'warning',
+        iconColor:'red',
+        confirmButtonText:"ตกลง",
+        confirmButtonColor:'green',
+      })
+      return;
+    }
+    if(person ==''){
+     // alert('กรุณาป้อนเงิน')
+      Swal.fire({
+        title:'คำเตือน',
+        text:'กรุณาป้อนคน',
+        icon:'warning',
+        iconColor:'red',
+        confirmButtonText:"ตกลง",
+        confirmButtonColor:'green',
+      })
+      return;
+    }
+    // คํานวณ
+    let result=parseFloat(moneyPay)/parseInt(person);
+    setMoneyShare(result.toLocaleString('th-TH'),{minimumFractionDigits:2,
+      maximumFractionDigits:2
+    });
+  }
+// สร้างฟังก์ชันยกเลิก
+  const handleCancelClick = () => {
+    setMoneyPay('');
+    setPerson('');
+    setMoneyShare('0.00');
+  }
   return (
     <div>
       <Navbariot />
@@ -16,20 +62,24 @@ export default function PageA() {
         </h1>
         <img src={marketShare} alt="รูปเงิน" className="w-30 mx-auto mt-5" />
         <h3 className="font-bold mt-5">ป้อนเงิน</h3>
-        <input type="number" placeholder="Input money ......" className="p-2 border boder-gray-300 rounded w-full mt-2"/>
-        <input type="number" placeholder="Input person ......" className="p-2 border boder-gray-300 rounded w-full mt-2"/>
+        <input value={moneyPay} onChange={(e)=>(setMoneyPay(e.target.value))}
+        type="number" placeholder="Input money ......" className="p-2 border boder-gray-300 rounded w-full mt-2"/>
+        <input value={person} onChange={(e)=>(setPerson(e.target.value))}
+        type="number" placeholder="Input person ......" className="p-2 border boder-gray-300 rounded w-full mt-2"/>
         <div className="flex mt-5">
-          <button className="bg-green-600 text-white p-3 rounded w-full mr-2 hover:bg-green-800 cursor-pointer">
+          <button onClick={handleCalMoneyShareClick}
+          className="bg-green-600 text-white p-3 rounded w-full mr-2 hover:bg-green-800 cursor-pointer">
             คำนวณ
             </button>
-          <button className="bg-red-600 text-white p-3 rounded w-full mr-2 hover:bg-red-800 cursor-pointer">
+          <button onClick={handleCancelClick}
+          className="bg-red-600 text-white p-3 rounded w-full mr-2 hover:bg-red-800 cursor-pointer">
             ยกเลิก
             </button>
         </div>
         <div className="flex mt-5 gap-5 justify-center items-center" >
             หารกันคนละ
             <span className="text-3xl text-red-500 font-bold">
-                0.00
+            {moneyShare}
             </span>
             บาท
 
